c4080e063633cdd7bbd2f72a3a1f8dc9cb09b532
reezyee <rezasapitra514@gmail.com>
2025-04-03 18:55:36 +0700

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
           content: "Website statis DPRD Tasikmalaya Kota",
         },
       ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "logo.ico" }],
     },
   },
 });
